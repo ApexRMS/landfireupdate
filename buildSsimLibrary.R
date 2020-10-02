5efcf03f48f395cf5c9ad9a0d834655604b2e78e
vlucet <VLucet@users.noreply.github.com>
2020-10-02 17:47:07 +0000

    add crosswlak

--- buildSsimLibrary.R
+++ buildSsimLibrary.R
@@ -9,7 +9,14 @@ library(rsyncrosim)
 library(RODBC)
 
 ## Load database
-landFireDB <- "db/NW_GeoArea_VegTransitions_Update_for_Remap_KCH_complete_2020_09_22.accdb"
+landFireDB <- 
+  "db/NW_GeoArea_VegTransitions_Update_for_Remap_KCH_complete_2020_09_22.accdb"
 db <- 
   odbcDriverConnect(paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=", 
                            landFireDB))
+
+## Load crosswalk 
+distCrosswalk <- 
+  read_csv("data/raw/non_spatial/LimUpdate2021_VDISTxFDIST_v02_20200925.csv")
+
+## Collect tables of interest
