d90be6f96d131557dc2fb17a07bbd2a02593e0fc
vlucet <VLucet@users.noreply.github.com>
2020-10-07 16:55:15 +0000

    remove masking

--- scripts/processSpatialData.R
+++ scripts/processSpatialData.R
@@ -27,20 +27,14 @@ nwEVHCropped <- crop(nwEVH, nwMapzones)
 nwEVCCropped <- crop(nwEVC, nwMapzones)
 fdistCropped <- crop(fdist, nwMapzones)
 
-## Mask after cropping
-nwEVTMasked <- mask(nwEVTCropped, nwMapzones)
-nwEVHMasked <- mask(nwEVHCropped, nwMapzones)
-nwEVCMasked <- mask(nwEVCCropped, nwMapzones)
-fdistMasked <- mask(fdistCropped, nwMapzones)
-
 ## Save clean data
-writeRaster(nwEVTMasked, "data/clean/nw_EVT_clean.tif",
+writeRaster(nwEVTCropped, "data/clean/nw_EVT_clean.tif",
             overwrite = TRUE)
-writeRaster(nwEVHMasked, "data/clean/nw_EVH_clean.tif",
+writeRaster(nwEVHCropped, "data/clean/nw_EVH_clean.tif",
             overwrite = TRUE)
-writeRaster(nwEVCMasked, "data/clean/nw_EVC_clean.tif",
+writeRaster(nwEVCCropped, "data/clean/nw_EVC_clean.tif",
             overwrite = TRUE)
-writeRaster(fdistMasked, "data/clean/nw_fDIST_clean.tif",
+writeRaster(fdistCropped, "data/clean/nw_fDIST_clean.tif",
             overwrite = TRUE)
 
 ## Crop data to smaller extent
@@ -49,10 +43,10 @@ theExt <- extent(-1417400, -1371131, 2614395, 2655111)
 
 # Crop
 nwMapzonesSmall <- crop(nwMapzones, theExt)
-nwEVTMaskedSmall <- crop(nwEVTMasked, theExt)
-nwEVHMaskedSmall <- crop(nwEVHMasked, theExt)
-nwEVCMaskedSmall <- crop(nwEVCMasked, theExt)
-fdistMaskedSmall <- crop(fdistMasked, theExt)
+nwEVTMaskedSmall <- crop(nwEVTCropped, theExt)
+nwEVHMaskedSmall <- crop(nwEVHCropped, theExt)
+nwEVCMaskedSmall <- crop(nwEVCCropped, theExt)
+fdistMaskedSmall <- crop(fdistCropped, theExt)
 
 ## Save cropped data
 writeRaster(nwMapzonesSmall, "data/clean/cropped/nw_Mapzones_small.tif",
