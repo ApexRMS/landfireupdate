642b7dbd719d8ff176f0e5c6e278642999678b3d
vlucet <VLucet@users.noreply.github.com>
2020-10-16 18:42:17 +0000

    cosmetic change + expand grid between stratum and state class

--- scripts/buildSsimLibrary.R
+++ scripts/buildSsimLibrary.R
@@ -4,19 +4,25 @@
 ### Extract tables from db and script library
 
 ##  Load packages
+
 library(tidyverse)
 library(rsyncrosim)
 library(raster)
 library(RODBC)
 
-## Load database
+## Access Database
+# Database path
+
 landFireDB <- 
   "db/NW_GeoArea_VegTransitions_Update_for_Remap_KCH_complete_2020_10_06.accdb"
+# Connect to database 
+
 db <- 
   odbcDriverConnect(paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=", 
                            landFireDB))
 
 ## Load crosswalk 
+
 distCrosswalk <- 
   read_csv("data/raw/non_spatial/LimUpdate2021_VDISTxFDIST_v02_20200925.csv") %>% 
   # Remove unimportant categories
@@ -25,8 +31,11 @@ distCrosswalk <-
   # Rename for easier handling
   rename(d_type_f = d_type_1, d_time_f = d_time_1)
 
-### Building Library
+
+# Build the SyncroSim Library ---------------------------------------------
+
 # Create library with a project ("Definitions") and a scenario ("Test")
+
 libraryName <- "LandFire_Test_SmallExtent.ssim"
 mylibrary <- ssimLibrary(paste0("library/", libraryName), overwrite = TRUE)
 myproject <- project(mylibrary, "Definitions", overwrite = TRUE)
@@ -53,12 +62,17 @@ transTbl <- sqlFetch(db, "vegtransf_rv02i_d") %>%
 # EVC and EVH
 
 EVClookup <- sqlFetch(db, "EVC_LUT") %>% 
+  # Turn factors into characters
   mutate_if(is.factor, as.character) %>% 
+  # Make unique names for when class names are repeated
   mutate(CLASSNAMES = ifelse(is.na(.$CLASSNAMES), 
                              paste0(.$EVT_LIFEFORM, "_", .$VALUE), 
                              .$CLASSNAMES)) %>% 
+  # Select relevant columns and rename with stsim relevant column names
   dplyr::select(VALUE, CLASSNAMES) %>% 
   rename(EVC_ID = VALUE, StateLabelXID = CLASSNAMES)
+
+# Similarly, for EVH:
 
 EVHlookup <- sqlFetch(db, "EVH_LUT") %>% 
   mutate_if(is.factor, as.character) %>%
@@ -69,32 +83,38 @@ EVHlookup <- sqlFetch(db, "EVH_LUT") %>%
   rename(EVH_ID = VALUE, StateLabelYID = CLASSNAMES)
 
 # JOIN Transitions to EVC/EVH
+
 transTblWithNames <- transTbl %>% 
   
+  # First jin EVC B and R, renaming appropriately
   left_join(EVClookup, by = c("EVCB" = "EVC_ID")) %>% 
   rename(EVCB_Name = StateLabelXID) %>% 
   left_join(EVClookup, by = c("EVCR" = "EVC_ID")) %>% 
   rename(EVCR_Name = StateLabelXID) %>%
   
+  # Similarly for EVH
   left_join(EVHlookup, by = c("EVHB" = "EVH_ID")) %>% 
   rename(EVHB_Name = StateLabelYID) %>% 
   left_join(EVHlookup, by = c("EVHR" = "EVH_ID")) %>% 
   rename(EVHR_Name = StateLabelYID) %>% 
   
+  # Create the State Class names from EVC : EVH combinations
   mutate(StateClassIDSource = paste0(EVCB_Name, " : ", EVHB_Name), 
          StateClassIDDest = paste0(EVCR_Name, " : ", EVHR_Name)) %>%
   
+  # Add the propability column with all set to 1
   mutate(Probability = 1)
 
-# Primary stratums 
+# Primary stratums (all unique EVT values)
 allEVT <- unique(transTbl$StratumIDSource)
 
-# Secondary stratum
+# Secondary stratum (all unique MZ values)
 allZones <- unique(transTbl$SecondaryStratumID)
 
-### DATASHEETS
+# Compose and load STSIM datasheets ---------------------------------------
 
 # Terminology
+
 term <- data.frame(
   AmountLabel = "Area", 
   AmountUnits = "Hectares", 
@@ -108,62 +128,86 @@ term <- data.frame(
 saveDatasheet(ssimObject = myproject, data = term, 
               name = "stsim_Terminology")
 
-## STRATUMS
+## Stratums
+# EVT is the primary stratum, MZ is the secondary stratum
+# No tertiary stratum
+# Important to take the unique values every time
 
 primary <- data.frame(ID = transTblWithNames$EVT7B, 
                       Name = transTblWithNames$StratumIDSource) %>% unique()
-# Colors
+
+# Extract colors from the evt200 sheet
+# TODO: An issue here where some IDs do not have colors and some colors do not
+# have matching IDs. 
+
 primaryWithColors <- sqlFetch(db, "nw_evt200") %>% 
+  # Select relevant columns
   dplyr::select(VALUE, R, G, B) %>% 
-  unique() %>% 
-  rename(ID = VALUE) %>% 
+  # Take unique and rename for later joining
+  unique() %>% rename(ID = VALUE) %>% 
+  # Create the color using the SyncroSim pattern of T, R, G, B
   mutate(Color = paste("255", R, G, B, sep = ",")) %>% 
+  # Join and select relevant columns
   right_join(primary, by = "ID") %>% 
   dplyr::select(ID, Color, Name)
-  
+
+# Save the datasheet
 saveDatasheet(myproject, primaryWithColors, "Stratum")
 
+# For secondary, no colors
 secondary <- data.frame(ID = transTblWithNames$MZ, 
                         Name = transTblWithNames$SecondaryStratumID) %>% unique()
 saveDatasheet(myproject, secondary, "SecondaryStratum")
 
-## STATE CLASSES
+## State Ckasses
 
 state_x <- data.frame(
   Name = EVClookup$StateLabelXID,
-  Description = EVClookup$StateLabelXID
-) %>% 
+  Description = EVClookup$StateLabelXID) %>% 
   unique()
+
 saveDatasheet(myproject, state_x, "stsim_StateLabelX")
 
 state_y <- data.frame(
   Name = EVHlookup$StateLabelYID,
-  Description = EVHlookup$StateLabelYID
-) %>% 
+  Description = EVHlookup$StateLabelYID) %>% 
   unique()
+
 saveDatasheet(myproject, state_y, "stsim_StateLabelY")
 
-# Make IDS
+# Make unique IDS
+# We do ECV * 1000 + EVH
+
 IDs <- c((transTblWithNames$EVCB*1000 + transTblWithNames$EVHB), 
          (transTblWithNames$EVCR*1000 + transTblWithNames$EVHR))
 
+# Build the datasheet
 stateClasses <- data.frame(
   ID = IDs,
   Name = c(transTblWithNames$StateClassIDSource, transTblWithNames$StateClassIDDest), 
   StateLabelXID = c(transTblWithNames$EVCB_Name, transTblWithNames$EVCR_Name), 
   StateLabelYID = c(transTblWithNames$EVHB_Name, transTblWithNames$EVHR_Name)) %>% 
   unique()
+
 saveDatasheet(myproject, stateClasses, "stsim_StateClass")
 
-## TRANSITION TYPES
+## Transition Types
+# We gather disturbance types from the VDIST table
+
 vdistLookup <-  sqlFetch(db, "VDIST") %>%
-  mutate_if(is.factor, as.character) %>% 
+  # Turn all factors into charactors
+  mutate_if(is.factor, as.character) %>%
+  # Select only what we need, then rename
   dplyr::select(value, d_type, d_severity, d_time, R, G, B) %>% 
   rename(ID = value,  TransitionGroupID = d_type) %>%
+  # Filter out the NO Disturbance category
   filter(ID != 0) %>%
+  # Create unique transition/disturbance name, and format color
+  # The format of the name is : Group, Severity, Frequency
   mutate(Name = paste(TransitionGroupID, d_severity, d_time, sep = ", ")) %>% 
   mutate(Color = paste("255", R, G, B, sep = ","))
 
+# Select what we want for the datasheet and save it
 transitionTypes <- vdistLookup %>% 
   dplyr::select(ID, Name, Color) %>% 
   unique()
@@ -172,6 +216,8 @@ saveDatasheet(myproject, transitionTypes, "stsim_TransitionType")
 
 ## Transition Groups
 ## TODO ask about IsAuto, dont remember what that is
+# For groups, we append the disturbance class to the existing datasheet
+
 TransitionGroups <- datasheet(myproject, "stsim_TransitionGroup") %>%
   mutate_if(is.factor, as.character) %>% 
   bind_rows(vdistLookup %>% 
@@ -181,6 +227,7 @@ TransitionGroups <- datasheet(myproject, "stsim_TransitionGroup") %>%
 saveDatasheet(myproject, TransitionGroups, "stsim_TransitionGroup")
 
 ## Transition Types by groups
+
 TypesByGroup <- vdistLookup %>% 
   dplyr::select(Name, TransitionGroupID) %>% 
   unique() %>% 
@@ -189,26 +236,39 @@ TypesByGroup <- vdistLookup %>%
 saveDatasheet(myproject, TypesByGroup, "stsim_TransitionTypeGroup")
 
 ## Transition simulation groups
+# The same than groups, used for vizualization
+
 SimulationGroups <- data.frame(
   TransitionGroupID = unique(vdistLookup$TransitionGroupID))
 
 saveDatasheet(myproject, SimulationGroups, "stsim_TransitionSimulationGroup")
 
-# SCENARIO TEST -----------------------------------------------------------
+# Create Test Scenario ----------------------------------------------------
 
-## TRANSITIONS
-# Determine what can be applied
+## Transitions
+# Because we are running under a smaller extent we first need to know what
+# disturbance type exist in this small extent
+
+# Load raster
 fDISTCropped <- raster("data/clean/cropped/nw_fDIST_clean_small.tif")
+EVTCropped <- raster("data/clean/cropped/nw_EVT_clean_small.tif")
+
+# Get all avlues
 allValues <- unique(fDISTCropped)
+allPrimaries <- unique(EVTCropped)
+
+# Filter for the types that are there
 transitionTypesCropped <- transitionTypes %>% 
   filter(ID %in% allValues)
+primaryFiltered <- primary %>% 
+  filter(ID %in% allPrimaries)
 
 # Deterministic
 # TODO specify the stratum here ??
 
-# Create the grid
-# This wrangles the locations of state classes in the UI
-# TODO come back here for the colors later, the df is already being splitted once
+# This wrangles the locations of state classes in the UI and stores thus 
+# information in a dataframe for joining later
+
 LocationDf <- stateClasses %>% 
   mutate(letter = ifelse(str_detect(StateLabelXID, "Tree"), "A", 
                          ifelse(str_detect(StateLabelXID, "Shrub"), "B",
@@ -222,57 +282,64 @@ LocationDf <- stateClasses %>%
 
 # Join the locations to the state classes df
 stateClassesjoined <- stateClasses %>% 
+  # Make sure to expand the grid to all combinations of MZ present (only 1 here)
+  expand_grid(StratumIDSource = primaryFiltered$Name) %>% 
+  # Join with the location info
   left_join(LocationDf, by = "Name")
 
-# Extract the columns for the datasheet
+# Extract/rename the columns to compose the datasheet and save it
 deterministicTransitions <- stateClassesjoined %>% 
-  dplyr::select(Name, Location) %>% 
+  dplyr::select(Name, Location, StratumIDSource) %>% 
   rename(StateClassIDSource = Name) %>% 
-  unique()
+  unique() %>% 
+  as.data.frame()
 
 saveDatasheet(myscenario, deterministicTransitions, 
               "stsim_DeterministicTransition") 
 
 # Probabilistic
 transTblWithNamesDatasheet <- transTblWithNames %>%
+  # Join the IDS
   left_join(transitionTypes, by = c("VDIST" = "ID")) %>% 
+  # Rename and select what we need 
   rename(TransitionTypeID = Name) %>% 
   dplyr::select(StratumIDSource, SecondaryStratumID,
                 StateClassIDSource, StateClassIDDest, 
                 TransitionTypeID, Probability)
 
+# Filter for values present in the smaller extent
 transTblWithNamesDatasheet <- transTblWithNamesDatasheet %>% 
   filter(TransitionTypeID %in% transitionTypesCropped$Name)
 
 saveDatasheet(myscenario, transTblWithNamesDatasheet, "stsim_Transition")
 
-## TRANSITION MULTIPLIERS
+## Transition multipliers
 
+# Collect the names and cretae path files
 multiplierGroupNames <- paste0(transitionTypesCropped$Name, " [Type]")
 multiplierFileNames <- paste0(getwd(), 
                               "/data/clean/cropped/FDIST/FDIST_value_", 
                               transitionTypesCropped$ID, ".tif")
 
+# Compose and save the data frame
 spatialMultiplier <- data.frame(
   TransitionGroupID = multiplierGroupNames,
-  MultiplierFileName = multiplierFileNames
-)
+  MultiplierFileName = multiplierFileNames)
 
 saveDatasheet(myscenario, spatialMultiplier, 
               "stsim_TransitionSpatialMultiplier")
 
-## INITIONAL CONDITIONS
+## Initial conditions
 
 initialConditionsSpatial <- data.frame(
   StateClassFileName = paste0(getwd(), "/data/clean/cropped/nw_EVC_EVH_StateClasses.tif"),
   StratumFileName = paste0(getwd(), "/data/clean/cropped/nw_Mapzones_small.tif"), 
-  SecondaryStratumFileName = paste0(getwd(), "/data/clean/cropped/nw_EVT_clean_small.tif") 
-)
+  SecondaryStratumFileName = paste0(getwd(), "/data/clean/cropped/nw_EVT_clean_small.tif"))
 
 saveDatasheet(myscenario, initialConditionsSpatial, 
               "stsim_InitialConditionsSpatial")
 
-## RUN CONTROL
+## Run Control
 
 runControl <- data.frame(
   MinimumIteration = 1,
